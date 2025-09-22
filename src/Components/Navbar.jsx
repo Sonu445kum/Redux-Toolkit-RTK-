e019e9afa4df8c13aa5224f6fc814305915829b6
Sonu445Kum <sonuroy1629@gmail.com>
2025-09-22 15:36:36 +0530

    Change the styling of the Navbar Components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Import the CSS file
+import "./Navbar.css"; 
 
 export default function Navbar() {
   const { totalQuantity } = useSelector((state) => state.cart);
